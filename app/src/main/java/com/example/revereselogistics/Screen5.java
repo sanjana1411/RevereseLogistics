dd09e657c22075d07d4f3ca7ccb63ab17956109a
vidhithebest <vidhi.jain2501@gmail.com>
2019-02-18 01:24:08 +0530

    Screen5 and screen6 are connected Screen main,2,5 formatted

--- app/src/main/java/com/example/revereselogistics/Screen5.java
+++ app/src/main/java/com/example/revereselogistics/Screen5.java
@@ -1,7 +1,10 @@
 package com.example.revereselogistics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Screen5 extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Screen5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_5);
+        Button wholesale_login = (Button) findViewById(R.id.wholesale_login);
+
+
+        wholesale_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent screen5;
+                screen5 = new Intent(Screen5.this, Screen6.class);
+                startActivity(screen5);
+            }
+        });
     }
 }

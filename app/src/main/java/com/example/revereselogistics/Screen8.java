2cb6162b55e2ef8f363e4f61638e507252c6af47
sanjanagoel <goel.sanjana14@gmail.com>
2019-02-18 00:49:17 +0530

    Screen 9 made

---
+++ app/src/main/java/com/example/revereselogistics/Screen8.java
@@ -0,0 +1,13 @@
+package com.example.revereselogistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen8);
+    }
+}

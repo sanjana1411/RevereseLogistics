1e0e49cd81b14c1bb7de7cc2e1984785256a1d40
sonalika sahoo <sonalikasahoo6@gmail.com>
2019-02-18 21:13:14 +0530

    .R issue resolved

--- app/src/main/java/com/example/revereselogistics/Screen1.java
+++ app/src/main/java/com/example/revereselogistics/Screen1.java
@@ -1,5 +1,6 @@
 package com.example.revereselogistics;
 
+import com.example.revereselogistics.*;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
